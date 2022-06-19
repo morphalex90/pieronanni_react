0be8207e4bff9d02e1354d2144be813a92328974
morphalex90 <piero.nanni@gmail.com>
2022-06-19 19:26:30 +0100

    add temp data jobs file

---
+++ src/components/Data/Jobs.jsx
@@ -0,0 +1,28 @@
+export const JobList = [
+    {
+        id: 2,
+        title: 'Web Developer',
+        company: {
+            name: 'Magic',
+            url: 'https://www.magicnet.it',
+        },
+        location: 'Bologna, Italy',
+        description: 'description magic',
+        description_cv: 'description cv',
+        start_date: '2015-05-01',
+        end_date: '2018-06-15',
+    },
+    {
+        id: 1,
+        title: '3D modeler and Web Programmer',
+        company: {
+            name: 'BackRaven',
+            url: 'https://www.blackravenproduction.com',
+        },
+        location: 'Bologna, Italy',
+        description: 'description black',
+        description_cv: 'description cv',
+        start_date: '2011-02-01',
+        end_date: '2015-08-01',
+    },
+];
