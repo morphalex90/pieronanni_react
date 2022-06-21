293e7ccc53a6779153551cdbb6948aaa01e2b085
morphalex90 <piero.nanni@gmail.com>
2022-06-21 01:40:26 +0100

    add global context to save default variables

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App'
 import './css/index.scss'
 
+import GlobalContextProvider from "./components/Context/GlobalContext";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+	<BrowserRouter>
+		<GlobalContextProvider>
+			<App />
+		</GlobalContextProvider>
+	</BrowserRouter>
 )
