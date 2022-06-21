4aa495ebad966831af0515cb9181db45f6905308
morphalex90 <piero.nanni@gmail.com>
2022-06-21 01:42:17 +0100

    add nav class

--- src/components/Layouts/Socials.jsx
+++ src/components/Layouts/Socials.jsx
@@ -1,6 +1,6 @@
 function Socials() {
     return (
-        <nav>
+        <nav className="socials-menu">
             <ul>
                 <li><a href="https://github.com/morphalex90" title="GitHub" target="_blank" rel="noopener noreferrer">GitHub</a></li>
                 <li><a href="https://www.linkedin.com/in/piero-nanni-87407193/?locale=en_US" title="LinkedIn" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
