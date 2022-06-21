48426531fa94cf09b98872bf932b2afb3f7572d4
morphalex90 <piero.nanni@gmail.com>
2022-06-21 01:40:41 +0100

    add initial projects

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,20 @@ import About from './components/Pages/About';
 import Projects from './components/Pages/Projects';
 import Contact from './components/Pages/Contact';
 import PrivacyPolicy from './components/Pages/PrivacyPolicy';
+import CV from './components/Pages/CV';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="about" element={<About />} />
-      <Route path="projects" element={<Projects />} />
-      <Route path="contact" element={<Contact />} />
-      <Route path="privacy-policy" element={<PrivacyPolicy />} />
-    </Routes>
-  )
+	return (
+		<Routes>
+			<Route path="/" element={<Homepage />} />
+			<Route path="about" element={<About />} />
+			<Route path="projects" element={<Projects />} />
+			<Route path="contact" element={<Contact />} />
+			<Route path="privacy-policy" element={<PrivacyPolicy />} />
+			<Route path="cv" element={<CV />} />
+
+		</Routes>
+	)
 }
 
 export default App
---
+++ src/components/Data/Projects.jsx
@@ -0,0 +1,57 @@
+export const ProjectsData = [
+    {
+        id: 1,
+        title: 'Animazione Le Piccole Pesti',
+        url: 'https://web.archive.org/web/20200806163843/https://www.animazionelepiccolepesti.it/',
+        technology: 'wp',
+        description: 'description Animazione le Piccole Pesti',
+        publish_date: '2020-04-12',
+        images: ['https://www.pieronanni.com/sites/default/files/styles/project/public/projects/storeanimazione-home.png']
+    },
+    {
+        id: 2,
+        title: 'Animazione Le Piccole Pesti react',
+        url: 'https://web.archive.org/web/20200806163843/https://www.animazionelepiccolepesti.it/',
+        technology: 'react',
+        description: 'description Animazione le Piccole Pesti',
+        publish_date: '2020-04-12',
+        images: ['https://www.pieronanni.com/sites/default/files/styles/project/public/projects/storeanimazione-home.png']
+    },
+    {
+        id: 3,
+        title: 'Animazione Le Piccole Pesti magento',
+        url: 'https://web.archive.org/web/20200806163843/https://www.animazionelepiccolepesti.it/',
+        technology: 'magento',
+        description: 'description Animazione le Piccole Pesti',
+        publish_date: '2020-04-12',
+        images: ['https://www.pieronanni.com/sites/default/files/styles/project/public/projects/storeanimazione-home.png']
+    },
+    {
+        id: 4,
+        title: 'Animazione Le Piccole Pesti',
+        url: 'https://web.archive.org/web/20200806163843/https://www.animazionelepiccolepesti.it/',
+        technology: 'wp',
+        description: 'description Animazione le Piccole Pesti',
+        publish_date: '2020-04-12',
+        images: ['https://www.pieronanni.com/sites/default/files/styles/project/public/projects/storeanimazione-home.png']
+    },
+    {
+        id: 5,
+        title: 'Animazione Le Piccole Pesti',
+        url: 'https://web.archive.org/web/20200806163843/https://www.animazionelepiccolepesti.it/',
+        technology: 'wp',
+        description: 'description Animazione le Piccole Pesti',
+        publish_date: '2020-04-12',
+        images: ['https://www.pieronanni.com/sites/default/files/styles/project/public/projects/storeanimazione-home.png']
+    },
+    {
+        id: 6,
+        title: 'Animazione Le Piccole Pesti',
+        url: 'https://web.archive.org/web/20200806163843/https://www.animazionelepiccolepesti.it/',
+        technology: 'wp',
+        description: 'description Animazione le Piccole Pesti',
+        publish_date: '2020-04-12',
+        images: ['https://www.pieronanni.com/sites/default/files/styles/project/public/projects/storeanimazione-home.png']
+    },
+    
+];
