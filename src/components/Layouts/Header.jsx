4b4a0c2754389befa568478253a3747af9c92abc
morphalex90 <piero.nanni@gmail.com>
2022-06-21 01:41:27 +0100

    remove logo, move socials below menu

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -3,26 +3,24 @@ import { Link } from "react-router-dom";
 import MainMenu from './MainMenu';
 import Socials from './Socials';
 
-// import logo from '../../img/logo.png';
-
 function Header() {
     return (
         <header className="header">
             <div className="header__container">
-                <div className="header__logo">
+                {/* <div className="header__logo">
                     <Link to="/">
-                        {/* <img src={logo} alt="Logo" title="Logo" /> */}
                         <span>{'<PieroNanni />'}</span>
                     </Link>
-                </div>
+                </div> */}
 
                 <div className="header__menu">
-                    <MainMenu />
-                </div>
-
-                <div className="header__socials">
+                    <MainMenu position="header" />
                     <Socials />
                 </div>
+
+                {/* <div className="header__socials">
+                    <Socials />
+                </div> */}
             </div>
         </header>
     );
