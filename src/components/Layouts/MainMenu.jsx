b2fea40e91d286e6c1f2d1204b74a869e8320aeb
morphalex90 <piero.nanni@gmail.com>
2022-06-21 01:41:44 +0100

    add Home link, ass menu position

--- src/components/Layouts/MainMenu.jsx
+++ src/components/Layouts/MainMenu.jsx
@@ -1,9 +1,10 @@
 import { NavLink } from "react-router-dom";
 
-function MainMenu() {
+function MainMenu(props) {
     return (
-        <nav role="navigation">
+        <nav role="navigation" className={'main-menu' + (props.position ? ' --' + props.position : '')}>
             <ul>
+                <li><NavLink to="/" className={({ isActive }) => isActive ? 'is-active' : undefined}>Home</NavLink></li>
                 <li><NavLink to="/about" className={({ isActive }) => isActive ? 'is-active' : undefined}>About</NavLink></li>
                 <li><NavLink to="/projects" className={({ isActive }) => isActive ? 'is-active' : undefined}>Projects</NavLink></li>
                 <li><NavLink to="/contact" className={({ isActive }) => isActive ? 'is-active' : undefined}>Contact</NavLink></li>
