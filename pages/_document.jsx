dfc2e36b32dfcf43fba5a1da12819f5685c8641e
morphalex90 <piero.nanni@gmail.com>
2022-10-05 21:16:34 +0100

    correct theme color

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -16,7 +16,7 @@ class MyDocument extends Document {
                     <link rel="manifest" href="/site.webmanifest" />
                     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
                     <meta name="msapplication-TileColor" content="#da532c" />
-                    <meta name="theme-color" content="#ffffff" />
+                    <meta name="theme-color" content="#000000" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                     <link href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&display=swap" rel="stylesheet" />                </Head>
