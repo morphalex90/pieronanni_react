74087c62963549faf01bdb8bd0cee20c6e974354
morphalex90 <piero.nanni@gmail.com>
2022-10-05 21:16:56 +0100

    make links goes purple if it's the current page

--- src/components/Layouts/MainMenu.jsx
+++ src/components/Layouts/MainMenu.jsx
@@ -1,13 +1,16 @@
 import Link from 'next/link';
+import { useRouter } from "next/router";
 
 function MainMenu(props) {
+    const router = useRouter();
+
     return (
         <nav role="navigation" className={'main-menu' + (props.position ? ' --' + props.position : '')}>
             <ul>
-                <li><Link href="/" className={({ isActive }) => isActive ? 'is-active' : undefined}><a>Home</a></Link></li>
-                <li><Link href="/about" className={({ isActive }) => isActive ? 'is-active' : undefined}><a>About</a></Link></li>
-                <li><Link href="/projects" className={({ isActive }) => isActive ? 'is-active' : undefined}><a>Projects</a></Link></li>
-                <li><Link href="/contact" className={({ isActive }) => isActive ? 'is-active' : undefined}><a>Contact</a></Link></li>
+                <li><Link href="/"><a title="Home">Home</a></Link></li>
+                <li><Link href="/about"><a className={router.pathname === '/about' ? 'is-active' : null} title="About">About</a></Link></li>
+                <li><Link href="/projects"><a className={router.pathname === '/projects' ? 'is-active' : null} title="Projects">Projects</a></Link></li>
+                <li><Link href="/contact"><a className={router.pathname === '/contact' ? 'is-active' : null} title="Contact">Contact</a></Link></li>
             </ul>
         </nav>
     );
