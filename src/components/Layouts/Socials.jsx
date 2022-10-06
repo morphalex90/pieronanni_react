0a93667f6b7917bcd17721e94a464f20c1891fa2
morphalex90 <piero.nanni@gmail.com>
2022-10-06 23:33:41 +0100

    make socials smaller

--- src/components/Layouts/Socials.jsx
+++ src/components/Layouts/Socials.jsx
@@ -6,8 +6,8 @@ function Socials() {
     return (
         <nav className="socials-menu">
             <ul>
-                <li><a href="https://github.com/morphalex90" title="GitHub" target="_blank" rel="noopener noreferrer"><Image src={github} alt={'GitHub'} title={'GitHub'} height={40} width={40} /> <span>GitHub</span></a></li>
-                <li><a href="https://www.linkedin.com/in/piero-nanni-87407193/?locale=en_US" title="LinkedIn" target="_blank" rel="noopener noreferrer"><span>LinkedIn</span> <Image src={linkedin} alt={'LinkedIn'} title={'LinkedIn'} height={40} width={40} /></a></li>
+                <li><a href="https://github.com/morphalex90" title="GitHub" target="_blank" rel="noopener noreferrer"><Image src={github} alt={'GitHub'} title={'GitHub'} height={30} width={30} /> <span>GitHub</span></a></li>
+                <li><a href="https://www.linkedin.com/in/piero-nanni-87407193/?locale=en_US" title="LinkedIn" target="_blank" rel="noopener noreferrer"><span>LinkedIn</span> <Image src={linkedin} alt={'LinkedIn'} title={'LinkedIn'} height={30} width={30} /></a></li>
             </ul>
         </nav>
     );
