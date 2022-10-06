d52779c22ccf2a47d8cbbc0dd12086f72f966e7f
morphalex90 <piero.nanni@gmail.com>
2022-10-06 22:29:02 +0100

    add custom message in return

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
+type Data = string;
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).send(
+    "You are definitely a curious one! Go back to the site!\n" +
+    " - homepage: https://www.pieronanni.com\n" +
+    " - about: https://www.pieronanni.com/about\n" +
+    " - projects: https://www.pieronanni.com/projects\n" +
+    " - contact: https://www.pieronanni.com/contact\n"
+  )
 }
