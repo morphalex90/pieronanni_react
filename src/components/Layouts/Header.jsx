55cf352c77896e4326bb42a833164f14735f365b
morphalex90 <piero.nanni@gmail.com>
2022-10-06 22:46:09 +0100

    remove old code

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,26 +1,18 @@
 // import { Link } from "react-router-dom";
 
-import MainMenu from './MainMenu';
-import Socials from './Socials';
+import MainMenu from '@layouts/MainMenu';
+import Socials from '@layouts/Socials';
 
 function Header() {
     return (
         <header className="header">
             <div className="header__container">
-                {/* <div className="header__logo">
-                    <Link to="/">
-                        <span>{'<PieroNanni />'}</span>
-                    </Link>
-                </div> */}
 
                 <div className="header__menu">
                     <MainMenu position="header" />
                     <Socials />
                 </div>
 
-                {/* <div className="header__socials">
-                    <Socials />
-                </div> */}
             </div>
         </header>
     );
