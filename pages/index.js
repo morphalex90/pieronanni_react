6e8826046ad11c272773b315947743a4db21d493
morphalex90 <piero.nanni@gmail.com>
2022-10-09 04:18:44 +0100

    correct meta description, make h1 and h2 animated separately

--- pages/index.js
+++ pages/index.js
@@ -10,22 +10,26 @@ function Homepage() {
             <Head>
                 <link rel="canonical" href={process.env.NEXT_PUBLIC_APP_URL} />
                 <title>Piero Nanni</title>
-                <meta name="description" content="PHP Developer in love with Drupal, London based" />
+                <meta name="description" content="PHP / Js Developer in love with Next.js, London based" />
 
                 <meta property="og:type" content="profile" />
                 <meta property="og:title" content="Piero Nanni" />
-                <meta property="og:description" content="PHP Developer in love with Drupal, London based" />
+                <meta property="og:description" content="PHP / Js Developer in love with Next.js, London based" />
                 <meta property="og:image" content="" />
                 <meta property="og:url" content={process.env.NEXT_PUBLIC_APP_URL} />
             </Head>
 
             <Layout className="homepage">
+                <section className="section --centered" style={{ height: 'calc(100vh - 57px - 26px)' }}>
+                    <motion.h1 initial={{ x: -50, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 0.2 }}>
+                        {'<PieroNanni/>'}
+                    </motion.h1>
 
-                <motion.section initial={{ x: -50, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 0.2 }} className="section --centered" style={{ height: 'calc(100vh - 56px - 26px)' }}>
-                    <h1>{'<PieroNanni/>'}</h1>
-                    <h2>Web Developer</h2>
-                </motion.section>
+                    <motion.h2 initial={{ x: 50, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 0.2 }}>
+                        Web Developer
+                    </motion.h2>
 
+                </section>
             </Layout>
         </>
     );
