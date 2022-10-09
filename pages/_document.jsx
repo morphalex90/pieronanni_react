7d16f017b69952205e4c4209877392eae8d2ed42
morphalex90 <piero.nanni@gmail.com>
2022-10-09 04:17:56 +0100

    fix issue on document file

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -18,7 +18,7 @@ class MyDocument extends Document {
                     <meta name="msapplication-TileColor" content="#da532c" />
                     <meta name="theme-color" content="#000000" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                     <link href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
