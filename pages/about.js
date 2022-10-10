d817a99720a0e5ebbb7e0aca6fe58f062702d339
morphalex90 <piero.nanni@gmail.com>
2022-10-10 18:35:31 +0100

    add text underline classes, add SP

--- pages/about.js
+++ pages/about.js
@@ -106,13 +106,15 @@ export default function About() {
 
                         <p>During high school I discovered the programming world: first with basic HTML, then I expanded my IT background over the years until I was able to create full web sites, for me or who was in need.</p>
 
-                        <p>From 2011 to 2015 I was part of an indie team based in Bologna, Italy (<a href="https://www.blackravenproduction.com/" target="_blank" rel="noreferrer" title="Visit Black Raven">Black Raven</a>), we developed small games and programs for Apple and PC. I was in charge of the design and the development of the website and of every methods of database connection of the apps. As a secondary role, I also worked as 3D modeller.<br />
+                        <p>From 2011 to 2015 I was part of an indie team based in Bologna, Italy (<a href="https://www.blackravenproduction.com/" className="t-underline" target="_blank" rel="noreferrer" title="Visit Black Raven">Black Raven</a>), we developed small games and programs for Apple and PC. I was in charge of the design and the development of the website and of every methods of database connection of the apps. As a secondary role, I also worked as 3D modeller.<br />
                             The years spent as part of this team enhanced my programming knowledge and made me learn the dynamics of working as part of a team. It is thanks to this experience that I have become the programmer that I am now.</p>
 
-                        <p>In May 2015 I&#39;ve got the chance to finally work in a web agency (<a href="https://www.magicnet.it/" target="_blank" rel="noreferrer" title="Visit Magic">Magic</a>) where I was trained about how companies develop websites and e-commerce platforms. Over these three years, I was able to build upon and expand my knowledge of WordPress, as well as discovering Drupal and Magento.</p>
+                        <p>In May 2015 I&#39;ve got the chance to finally work in a web agency (<a href="https://www.magicnet.it/" className="t-underline" target="_blank" rel="noreferrer" title="Visit Magic">Magic</a>) where I was trained about how companies develop websites and e-commerce platforms. Over these three years, I was able to build upon and expand my knowledge of WordPress, as well as discovering Drupal and Magento.</p>
 
                         <p>In June 2018 I&#39;ve moved to London in order to expand my knowledge and increase my english level.<br />
-                            After a couple of months I joined <a href="https://purrgroup.com/" target="_blank" rel="noreferrer" title="Visit Purr">Purr</a>, a web agency based in central London and since then there have been lots of many interesting projects and new ways for building websites that I&#39;ve never explored</p>
+                            After a couple of months I joined <a href="https://purrgroup.com/" className="t-underline" target="_blank" rel="noreferrer" title="Visit Purr">Purr</a>, a web agency based in central London and since then there have been lots of many interesting projects and new ways for building websites that I&#39;ve never explored</p>
+
+                        <p>Add about <a href="https://www.soundpickr.com/" className="t-underline" target="_blank" rel="noreferrer" title="Visit Soundpickr">Soundpickr</a></p>
                     </motion.section>
                 </div>
             </Layout>
