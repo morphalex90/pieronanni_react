433af6bd03524031a46bc7c7148c7feddeef958a
morphalex90 <piero.nanni@gmail.com>
2022-10-10 18:33:00 +0100

    remove IDs

--- src/components/Data/Technologies.jsx
+++ src/components/Data/Technologies.jsx
@@ -1,46 +1,37 @@
 export const TechnologiesData = [
     {
-        id: 1,
         name: 'All',
         key: '*',
     },
     {
-        id: 2,
         name: 'Drupal 7',
         key: 'd7',
     },
     {
-        id: 3,
         name: 'Drupal 8',
         key: 'd8',
     },
     {
-        id: 4,
         name: 'Drupal 9',
         key: 'd9',
     },
     {
-        id: 5,
         name: 'HTML',
         key: 'html5',
     },
     {
-        id: 6,
         name: 'Magento',
         key: 'magento',
     },
     {
-        id: 7,
         name: 'React',
         key: 'react',
     },
     {
-        id: 8,
         name: 'WordPress',
         key: 'wp',
     },
     {
-        id: 9,
         name: 'Laravel',
         key: 'laravel',
     },
