d8faf6f1e378f49f8a814aa87eef84c8a98ffc8f
morphalex90 <piero.nanni@gmail.com>
2022-12-13 18:09:14 +0000

    add job descriptions and images

--- src/components/Data/Jobs.jsx
+++ src/components/Data/Jobs.jsx
@@ -16,7 +16,7 @@ export const JobList = [
                 title: 'Soundpickr',
                 url: 'https://www.soundpickr.com',
                 technologies: ['react', 'laravel'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '<p>description Soundpickr to do!</p>',
                 publish_date: '2020-04-12',
                 images: ['soundpickr.webp']
             },
@@ -39,31 +39,31 @@ export const JobList = [
                 title: 'Mathys & Squire - Scalehub Quarter',
                 url: 'https://www.mathys-squire.com/scaleup-quarter/',
                 technologies: ['wp'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '',
                 publish_date: '2020-04-12',
-                images: ['mathys-squire_scaleup-quarter.png']
+                images: ['mathys-squire_scaleup-quarter.png', 'mathys-squire_scaleup-quarter_strateby_investments.png']
             },
             {
                 title: 'Elder',
                 url: 'https://www.elder.org/',
                 technologies: ['wp'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '',
                 publish_date: '2020-04-12',
-                images: ['elder.png']
+                images: ['elder.png', 'elder-live-in-care.png']
             },
             {
                 title: 'Mathys & Squire',
                 url: 'https://www.mathys-squire.com/',
                 technologies: ['wp'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '',
                 publish_date: '2020-04-12',
-                images: ['mathys-squire.png']
+                images: ['mathys-squire.png', 'mathys-people.png', 'mathys-sectors.png']
             },
             {
                 title: 'AHSNNetwork',
                 url: 'https://www.ahsnnetwork.com/',
                 technologies: ['wp'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '',
                 publish_date: '2020-04-12',
                 images: ['ahsnnetwork.png']
             },
@@ -71,7 +71,7 @@ export const JobList = [
                 title: 'Brompton',
                 url: 'https://www.bromptontech.com/',
                 technologies: ['wp'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '',
                 publish_date: '2020-04-12',
                 images: ['brompton.png']
             },
@@ -79,9 +79,9 @@ export const JobList = [
                 title: 'Stephenson Harwood',
                 url: 'https://www.offshoreenergylaw.com/',
                 technologies: ['d9'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '',
                 publish_date: '2020-04-12',
-                images: ['stephenson_harwood.png']
+                images: ['stephenson_harwood.png', 'stephenson_harwood_2.png', 'stephenson_harwood_3.png', 'stephenson_harwood_4.png', 'stephenson_harwood_5.png', 'stephenson_harwood_6.png']
             },
         ],
     },
@@ -102,7 +102,7 @@ export const JobList = [
                 title: 'Piero Nanni.me',
                 url: 'https://www.pieronanni.me',
                 technologies: ['react'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '',
                 publish_date: '2022-11-01',
                 images: ['pieronanni_me.png']
             },
@@ -110,7 +110,7 @@ export const JobList = [
                 title: 'SEO Tools',
                 url: 'https://tools.morpheus90.com',
                 technologies: ['react', 'laravel'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '<p>During the develop&nbsp;of my websites I realized that I was missing a tool that would group the functions I use the most.</p><p>After digging the net looking for something useful and not founding something that pleased me, I\'ve started developing this website where I could insert all the tools I needed, in particular:</p><ul><li>check of title and alt on images</li><li>check of all the headings</li><li>check of all the meta datas</li><li>check of the file robots.txt and it\'s content</li><li>check of the sitemap existence and it\'s content</li></ul><p>After lots of tests and fixes, I\'ve finally released the first version built in pure HTML and Javascript / jQuery.</p><p>A first rebuild was made, by converting the project into a full Laravel installation&nbsp;in order to have better structure, but still I was not satisfied.</p><p>Then a second rebuild was made, this time with a combination of React in front-end (published in Netlify) and a Lumen back-end hosted into my server.<br />I\'ve choose this combination during my initial studies with React because I find that React is pretty fast and the back-end is only called by API when needed.<br />The final result is really fast compared to the previous 2 and this time I\'m quite happy with this version.</p>',
                 publish_date: '2020-07-02',
                 images: ['tools.png']
             },
@@ -118,7 +118,7 @@ export const JobList = [
                 title: 'Tatis\'s Wedding',
                 url: 'https://www.tatiswedding.it/',
                 technologies: ['react'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '',
                 publish_date: '2020-05-13',
                 images: ['tatiswedding.png']
             },
@@ -126,15 +126,15 @@ export const JobList = [
                 title: 'Store Animazione le Piccole Pesti',
                 url: 'https://web.archive.org/web/20210411065823/https://store.animazionelepiccolepesti.it/',
                 technologies: ['d8'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '',
                 publish_date: '2020-05-13',
-                images: ['storeanimazione-home.png']
+                images: ['storeanimazione-home.png', 'storeanimazione-category.png', 'storeanimazione-laboratori.png', 'storeanimazione-promozioni.png']
             },
             {
                 title: 'Animazione le Piccole Pesti',
                 url: 'https://web.archive.org/web/20200806163843/https://www.animazionelepiccolepesti.it/',
                 technologies: ['wp'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '<p>Animazione Le Piccole Pesti is the commerce activity for children entertainment during birthdays, weddings, etc... created by a good friend of mine.</p><p>The site has been rebuild in order to have a better look compared to the old design, and lots of pages has been deleted and/or regrouped, in order to make it simpler from users and to gather informations.</p><p>It\'s based on Gutenberg blocks in order to provide a better back-end experience.</p><p>A Drupal based <a href="https://store.animazionelepiccolepesti.it/" target="_blank" title="Visit Animazione Le Piccole Pesti e-commerce">e-commerce</a> has been built in order to sell online the merchandising sold on the shop</p>',
                 publish_date: '2020-04-12',
                 images: ['animazione.png']
             },
@@ -142,7 +142,7 @@ export const JobList = [
                 title: 'Cronometro.info',
                 url: 'https://www.tatiswedding.it/',
                 technologies: ['html5'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '<p>Project born with the purpose of showing my js/jQuery knowledge and of testing how Google AdWords works (still to implement)</p><p>The result is a simple stopwatch that can handle partial timings.</p><p>It has been rebuilt in 2020 by using only vanilla js and drop the dependency on jQuery</p>',
                 publish_date: '2018-03-19',
                 images: ['cronometro.png']
             },
@@ -150,7 +150,7 @@ export const JobList = [
                 title: 'Piero Nanni.com',
                 url: 'https://www.pieronanni.com',
                 technologies: ['d9'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '<p>This website starts as a project at the beginning of 2017 when I wanted to pass from my old domain morpheus90.com to something much more professional, so I bought pieronanni.it and pieronanni.com domains, then installed Drupal 8 and finally started to add all my works.</p><p>The first version came out in march 2018 and it was created by using a custom child theme of Bootstrap, it was ok at that time even if having such a dependency was not the best.</p><p>Time passes and I finally learn how to fully create a theme from& scratch, then I\'ve rebuild this site in order to give a better look and be way more optimized. I am really proud of the result!</p>',
                 publish_date: '2018-03-02',
                 images: ['pieronanni-homepage.png', 'pieronanni-about.png', 'pieronanni-projects.png', 'pieronanni-contact.png']
             },
@@ -158,7 +158,7 @@ export const JobList = [
                 title: 'La Buona Bottega',
                 url: 'https://www.labuonabottega.it',
                 technologies: ['d8'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '<p>Ecommerce of product eco friendly created with Drupal 8 and the Ubercart module</p><p>It has been created as much content types as much product types we have, so it\'s possible to completly customize both fields and template/block, in addition to have the total control from backoffice for the meta managment for every single product</p><p>The theme is based on Bootstrap and it has been created a child theme in order to override it</p><p>Lots of private pages has been created in order to add tools for the editor user in order to manage the content creation and the handling of products/users/orders</p>',
                 publish_date: '2017-09-01',
                 images: ['labuonabottega.png']
             },
@@ -166,7 +166,7 @@ export const JobList = [
                 title: 'Sara Anna Nadalini',
                 url: 'https://www.saraannanadalini.com',
                 technologies: ['react'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '<p>This is a static website based on WordPress used as a project and works collection for the content creator Sara Anna Nadalini</p><p>The theme is based on one of the basic WordPress theme, completly revisited in order to make it 100% customized with&nbsp;the owner wishes</p><p>This is one of the fastest WordPress website I\'ve ever build, thanks to the fact that there are very few contents and lots of external links</p>',
                 publish_date: '2016-03-20',
                 images: ['sara-anna-nadalini.png']
             },
@@ -300,7 +300,7 @@ export const JobList = [
                 title: 'BlackRaven',
                 url: 'https://www.blackravenproduction.com/',
                 technologies: ['wp'],
-                description: '<p>description Soundpickr first!</p>',
+                description: '<p>I\'ve been part of this indie team since 2011 until the closing in 2015, my role was web designer and 3D modeller.</p><p>In this website (and in the <a href="https://www.facebook.com/Black-Raven-274328735978223/" target="_blank" title="Check out the Facebook page"> Facebook page</a>) we have included all the products that we developed during this years, all the news of the exibitions we participed and all the events we went</p>',
                 publish_date: '2020-04-12',
                 images: ['blackraven.png']
             },
