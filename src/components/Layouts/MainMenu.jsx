c7deb8a52ee30e7b22acb31c7e8d0321a688517e
morphalex90 <piero.nanni@gmail.com>
2022-12-13 15:01:05 +0000

    show GitHub and LinkedIn only on header menu

--- src/components/Layouts/MainMenu.jsx
+++ src/components/Layouts/MainMenu.jsx
@@ -11,12 +11,17 @@ function MainMenu(props) {
     return (
         <nav className={'main-menu' + (props.position ? ' --' + props.position : '')}>
             <ul>
-                <li className="has-icon"><a href="https://github.com/morphalex90" title="GitHub" target="_blank" rel="noopener noreferrer"><Image src={github} alt={'GitHub'} title={'GitHub'} height={26} width={26} /></a></li>
+                {props.position === 'header' &&
+                    <li className="has-icon"><a href="https://github.com/morphalex90" title="GitHub" target="_blank" rel="noopener noreferrer"><Image src={github} alt={'GitHub'} title={'GitHub'} height={26} width={26} /></a></li>
+                }
                 <li><Link href="/" title="Home">Home</Link></li>
                 <li><Link href="/about" className={router.pathname === '/about' ? 'is-active' : null} title="About">About</Link></li>
                 <li><Link href="/projects" className={router.pathname === '/projects' ? 'is-active' : null} title="Projects">Projects</Link></li>
                 <li><Link href="/contact" className={router.pathname === '/contact' ? 'is-active' : null} title="Contact">Contact</Link></li>
-                <li className="has-icon"><a href="https://www.linkedin.com/in/piero-nanni-87407193/?locale=en_US" title="LinkedIn" target="_blank" rel="noopener noreferrer"><Image src={linkedin} alt={'LinkedIn'} title={'LinkedIn'} height={26} width={26} /></a></li>
+
+                {props.position === 'header' &&
+                    <li className="has-icon"><a href="https://www.linkedin.com/in/piero-nanni-87407193/?locale=en_US" title="LinkedIn" target="_blank" rel="noopener noreferrer"><Image src={linkedin} alt={'LinkedIn'} title={'LinkedIn'} height={26} width={26} /></a></li>
+                }
             </ul>
         </nav>
     );
