8f009148f9896b9a233c8b218a34c46535078baf
morphalex90 <piero.nanni@gmail.com>
2023-04-05 17:19:23 +0100

    correct url

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,9 +9,9 @@ export default function handler(
 ) {
   res.status(200).send(
     "You are definitely a curious one! Go back to the site!\n" +
-    " - homepage: https://www.pieronanni.com\n" +
-    " - about: https://www.pieronanni.com/about\n" +
-    " - projects: https://www.pieronanni.com/projects\n" +
-    " - contact: https://www.pieronanni.com/contact\n"
+    " - homepage: https://www.pieronanni.me\n" +
+    " - about: https://www.pieronanni.me/about\n" +
+    " - projects: https://www.pieronanni.me/projects\n" +
+    " - contact: https://www.pieronanni.me/contact\n"
   )
 }
