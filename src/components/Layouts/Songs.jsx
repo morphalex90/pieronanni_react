077309dc2d741a232aa60a2cbc23909e5e4b4b8e
morphalex90 <piero.nanni@gmail.com>
2023-04-28 13:14:02 +0100

    convert to use new structure

--- src/components/Layouts/Songs.jsx
+++ src/components/Layouts/Songs.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 export default function ShowSongs() {
     const [songs, setSongs] = useState([]);
@@ -51,7 +51,7 @@ export default function ShowSongs() {
         })
             .then(response => response.json())
             .then(data => {
-                data.map(song => {
+                data.songs.map(song => {
                     // console.log(song);
                     // setSongs([...songs, { image_url: song.image_url, song_url: song.song_url }])
                     setSongs(prevState => [{ name: song.name, artist: song.song_artist, image_url: song.image_url, song_url: song.song_url + '?api_key=' + process.env.NEXT_PUBLIC_SP_API_KEY }, ...prevState]);
@@ -63,41 +63,36 @@ export default function ShowSongs() {
     }
 
     return (
-        <>
-            {songs.length !== 0 &&
-                <div className="player">
-                    <div className="player__top">
-                        <Image src={songs[selectedSong].image_url} alt={songs[selectedSong].name} height={100} width={100} />
-                        <br />
-                        <br />
 
-                        <div className="player__top__controls">
-                            <div className="player__top__controls__prev" onClick={() => setSelectedSong(selectedSong === 0 ? selectedSong : (selectedSong - 1))}>&lt;</div>
+        songs.length !== 0 &&
+        <div className="player">
+            <div className="player__top">
+                <img src={songs[selectedSong].image_url} alt={songs[selectedSong].name} height={100} width={100} />
+                <br />
+                <br />
 
-                            <audio controls controlsList="nodownload noplaybackrate nodownload" preload="metadata" ref={audioRef}>
-                                <source src={songs[selectedSong].song_url} type="audio/mpeg" />
-                                Your browser does not support the audio element.
-                            </audio>
+                <div className="player__top__controls">
+                    <div className="player__top__controls__prev" onClick={() => setSelectedSong(selectedSong === 0 ? selectedSong : (selectedSong - 1))}>&lt;</div>
 
-                            <div className="player__top__controls__next" onClick={() => setSelectedSong(selectedSong === (songs.length - 1) ? 0 : (selectedSong + 1))}>&gt;</div>
-                        </div>
+                    <audio controls controlsList="nodownload noplaybackrate nodownload" preload="metadata" ref={audioRef}>
+                        <source src={songs[selectedSong].song_url} type="audio/mpeg" />
+                        Your browser does not support the audio element.
+                    </audio>
 
-                    </div>
-                    <div className="player__list">
-                        {songs.length !== 0 &&
-                            songs.map((song, id) => {
-                                return (
-                                    <div key={id} className={'player__list__song' + (selectedSong === id ? ' --selected' : '')} onClick={() => setSelectedSong(id)}>
-                                        <div className="player__list__song__name">{song.name}</div>
-                                        <div className="player__list__song__artist">{song.artist?.name}</div>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-
+                    <div className="player__top__controls__next" onClick={() => setSelectedSong(selectedSong === (songs.length - 1) ? 0 : (selectedSong + 1))}>&gt;</div>
                 </div>
-            }
-        </>
+
+            </div>
+            <div className="player__list">
+                {songs.length !== 0 &&
+                    songs.map((song, id) =>
+                        <div key={id} className={'player__list__song' + (selectedSong === id ? ' --selected' : '')} onClick={() => setSelectedSong(id)}>
+                            <div className="player__list__song__name">{song.name}</div>
+                            <div className="player__list__song__artist">{song.artist?.name}</div>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
     );
 }
