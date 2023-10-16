715cd36273c99bcf62c6e4ff5442c71a86e7080c
morphalex90 <piero.nanni@gmail.com>
2023-10-16 13:28:11 +0100

    redirect cv to cv.pdf

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/cv',
-  //       destination: '/cv.pdf',
-  //       permanent: true,
-  //     },
-  //   ]
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/cv',
+        destination: '/cv.pdf',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
