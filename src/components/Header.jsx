244a4d77bd5427705b40317a849c641e65ac15a5
morphalex90 <piero.nanni@gmail.com>
2023-11-27 17:48:39 +0000

    different menu in mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,30 @@
+import { useState } from 'react';
 import MainMenu from '@layouts/MainMenu';
 
 export default function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openMobileMenu = () => {
+        // have the body not scrollable when the menu is open
+        if (isOpen) {
+            document.body.classList.remove('--open');
+        } else {
+            document.body.classList.add('--open');
+        }
+
+        setIsOpen(!isOpen); // open / close the menu
+    }
+
     return (
         <header className="header">
             <div className="header__container">
 
                 <div className="header__menu">
                     <MainMenu position="header" />
+                </div>
+
+                <div className="header__burger">
+                    <button type="button" className="header__burger__icon" onClick={() => openMobileMenu()}></button>
                 </div>
 
             </div>
