ca3ef99e22aea0eab8be16adf22585e4a48d2e68
morphalex90 <piero.nanni@gmail.com>
2023-12-12 09:35:24 +0000

    fix 404 page inner height to be vertically centered

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -21,7 +21,7 @@ export default function Page404() {
             </Head>
 
             <Layout className="page-404">
-                <motion.section initial={{ x: -50, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 0.2 }} className="section --centered" style={{ height: 'calc(100vh - 56px - 26px)' }}>
+                <motion.section initial={{ x: -50, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 0.2 }} className="section --centered">
                     <h1>404 - Page not Found</h1>
                     <h2>Try again, you might get lucky</h2>
                     <MainMenu />
