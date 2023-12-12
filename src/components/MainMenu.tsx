a3705ad50c70910c41d93d1536e30906c665bfd9
morphalex90 <piero.nanni@gmail.com>
2023-12-12 09:30:47 +0000

    convert to typescript and add a default value for position

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import github from '@img/github.svg';
 import linkedin from '@img/linkedin.png';
 
-export default function MainMenu({ position }) {
+export default function MainMenu({ position = '' }) {
     const router = useRouter();
 
     return (
@@ -14,9 +14,9 @@ export default function MainMenu({ position }) {
                     <li className="has-icon"><a href="https://github.com/morphalex90" title="GitHub" target="_blank" rel="noopener noreferrer"><Image src={github} alt="GitHub" title="GitHub" height={26} width={26} priority /></a></li>
                 }
                 <li><Link href="/" title="Home">Home</Link></li>
-                <li><Link href="/about" className={router.pathname === '/about' ? 'is-active' : null} title="About">About</Link></li>
-                <li><Link href="/projects" className={router.pathname === '/projects' ? 'is-active' : null} title="Projects">Projects</Link></li>
-                <li><Link href="/contact" className={router.pathname === '/contact' ? 'is-active' : null} title="Contact">Contact</Link></li>
+                <li><Link href="/about" className={router.pathname === '/about' ? 'is-active' : ''} title="About">About</Link></li>
+                <li><Link href="/projects" className={router.pathname === '/projects' ? 'is-active' : ''} title="Projects">Projects</Link></li>
+                <li><Link href="/contact" className={router.pathname === '/contact' ? 'is-active' : ''} title="Contact">Contact</Link></li>
                 <li><Link href="/cv.pdf" title="cv">CV</Link></li>
 
                 {position === 'header' &&
