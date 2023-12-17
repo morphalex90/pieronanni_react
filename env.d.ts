f5a87d7377fcbc944b5bd2b1a5f4d7ce06175925
morphalex90 <piero.nanni@gmail.com>
2023-12-17 15:23:20 +0000

    install and initialise posthog

---
+++ env.d.ts
@@ -0,0 +1,5 @@
+namespace NodeJS {
+    interface ProcessEnv {
+        NEXT_PUBLIC_POSTHOG_KEY: string;
+    }
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,37 @@
 import '@css/index.scss';
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
 import { Fira_Mono } from 'next/font/google'
 
 const firaMono = Fira_Mono({ weight: ['400', '500', '700'], subsets: ['latin'] })
 
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+    persistence: 'memory',
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug()
+    }
+  })
+}
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = () => posthog?.capture('$pageview')
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
   return (
     <>
       {/* eslint-disable react/no-unknown-property */}
@@ -13,7 +40,9 @@ export default function App({ Component, pageProps }: AppProps) {
 				font-family: ${firaMono.style.fontFamily};
 				}
 			`}</style>
-      <Component {...pageProps} />
+      <PostHogProvider client={posthog}>
+        <Component {...pageProps} />
+      </PostHogProvider>
     </>
   )
 }
